f838915cc0fd766532e20399fabd3f2431c17c24
shardayyyy <shardayyy@gmail.com>
2018-10-08 22:00:55 -0400

    complete

---
+++ test_data/insert.sql
@@ -0,0 +1,138 @@
+INSERT INTO OP_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (1, "ACWMAK", True, "349", "2018-10-08");
+
+
+INSERT INTO OP_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (2, "XETAXT", False, "026", "2018-10-08");
+
+
+INSERT INTO OP_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (3, "PFQMEM", False, "319", "2018-10-08");
+
+
+INSERT INTO OP_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (4, "QLXTES", False, "035", "2018-10-08");
+
+
+INSERT INTO OP_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (5, "DUXHGU", True, "203", "2018-10-08");
+
+
+INSERT INTO OP_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (6, "DRZSUE", False, "177", "2018-10-08");
+
+
+INSERT INTO OP_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (7, "GQPUZH", True, "961", "2018-10-08");
+
+
+INSERT INTO OP_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (8, "YJBCAW", False, "638", "2018-10-08");
+
+
+INSERT INTO OP_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (9, "DRKNSF", False, "640", "2018-10-08");
+
+
+INSERT INTO OP_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (10, "HYQFOO", False, "062", "2018-10-08");
+
+
+INSERT INTO FCT_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (1, "ETSNLH", False, "286", "2018-10-08");
+
+
+INSERT INTO FCT_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (2, "ICDUDV", False, "344", "2018-10-08");
+
+
+INSERT INTO FCT_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (3, "ULXACR", False, "804", "2018-10-08");
+
+
+INSERT INTO FCT_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (4, "FJXTQR", True, "062", "2018-10-08");
+
+
+INSERT INTO FCT_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (5, "QZXWIV", False, "666", "2018-10-08");
+
+
+INSERT INTO FCT_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (6, "HDEMGJ", False, "908", "2018-10-08");
+
+
+INSERT INTO ET_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (1, "EJPAXZ", False, "960", "2018-10-08");
+
+
+INSERT INTO ET_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (2, "GUDJUR", False, "792", "2018-10-08");
+
+
+INSERT INTO ET_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (3, "CJMFBZ", False, "783", "2018-10-08");
+
+
+INSERT INTO ET_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (4, "YMJUBZ", True, "988", "2018-10-08");
+
+
+INSERT INTO ET_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (5, "KPJXUO", False, "908", "2018-10-08");
+
+
+INSERT INTO ET_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (6, "EEQBYA", True, "937", "2018-10-08");
+
+
+INSERT INTO EU_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (1, "RDECGQ", True, "247", "2018-10-08");
+
+
+INSERT INTO EU_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (2, "QNJKGG", False, "235", "2018-10-08");
+
+
+INSERT INTO EU_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (3, "UUTAXT", True, "469", "2018-10-08");
+
+
+INSERT INTO EU_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (4, "ZSIQJK", True, "100", "2018-10-08");
+
+
+INSERT INTO EU_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (5, "QDGDQS", True, "801", "2018-10-08");
+
+
+INSERT INTO EU_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (6, "BSKYVR", True, "966", "2018-10-08");
+
+
+INSERT INTO PM_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (1, "THIIUO", False, "321", "2018-10-08");
+
+
+INSERT INTO PM_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (2, "ZLPLYP", False, "740", "2018-10-08");
+
+
+INSERT INTO PM_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (3, "ULWLND", True, "569", "2018-10-08");
+
+
+INSERT INTO PM_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (4, "OHJEWL", False, "847", "2018-10-08");
+
+
+INSERT INTO PM_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (5, "OAYEQV", True, "221", "2018-10-08");
+
+
+INSERT INTO PM_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (6, "PMZJSP", False, "499", "2018-10-08");
+
+
+INSERT INTO CFI_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (1, "PAMQPH", False, "104", "2018-10-08");
+
+
+INSERT INTO CFI_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (2, "JRXPKP", False, "416", "2018-10-08");
+
+
+INSERT INTO CFI_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (3, "ECNKCI", True, "792", "2018-10-08");
+
+
+INSERT INTO CFI_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (4, "LVSRYE", False, "179", "2018-10-08");
+
+
+INSERT INTO CFI_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (5, "OTVPZT", False, "582", "2018-10-08");
+
+
+INSERT INTO CFI_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (6, "XCJBDB", True, "465", "2018-10-08");
+
+
+INSERT INTO FL_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (1, "ZNFZMZ", False, "079", "2018-10-08");
+
+
+INSERT INTO FL_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (2, "GZOBID", False, "430", "2018-10-08");
+
+
+INSERT INTO FL_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (3, "ABOSTT", False, "072", "2018-10-08");
+
+
+INSERT INTO FL_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (4, "QTPSDR", False, "099", "2018-10-08");
+
+
+INSERT INTO FL_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (5, "WSMSGQ", True, "552", "2018-10-08");
+
+
+INSERT INTO FL_TABLE_NAME("column_name1", "column_name2", "column_name3", "column_name4", "column_name5") values (6, "ZSBLXW", True, "974", "2018-10-08");
+
+
--- test_data/test.py
+++ test_data/test.py
@@ -1,50 +1,52 @@
 import string
 import random
+import datetime
+from collections import OrderedDict
 
-op_dict = {}
-op_dict['colunm_name1'] = 'int'
-op_dict['colunm_name2'] = 'varchar'
-op_dict['colunm_name3'] = 'boolean'
-op_dict['colunm_name4'] = 'number'
-op_dict['colunm_name5'] = 'date'
+op_dict =OrderedDict()
+op_dict['column_name1'] = 'pk'
+op_dict['column_name2'] = 'varchar'
+op_dict['column_name3'] = 'boolean'
+op_dict['column_name4'] = 'number'
+op_dict['column_name5'] = 'date'
 
-fct_dict = {}
-fct_dict['colunm_name1'] = 'int'
-fct_dict['colunm_name2'] = 'varchar'
-fct_dict['colunm_name3'] = 'boolean'
-fct_dict['colunm_name4'] = 'number'
-fct_dict['colunm_name5'] = 'date'
+fct_dict =OrderedDict()
+fct_dict['column_name1'] = 'pk'
+fct_dict['column_name2'] = 'varchar'
+fct_dict['column_name3'] = 'boolean'
+fct_dict['column_name4'] = 'number'
+fct_dict['column_name5'] = 'date'
 
-et_dict = {}
-et_dict['colunm_name1'] = 'int'
-et_dict['colunm_name2'] = 'varchar'
-et_dict['colunm_name3'] = 'boolean'
-et_dict['colunm_name4'] = 'number'
-et_dict['colunm_name5'] = 'date'
+et_dict =OrderedDict()
+et_dict['column_name1'] = 'pk'
+et_dict['column_name2'] = 'varchar'
+et_dict['column_name3'] = 'boolean'
+et_dict['column_name4'] = 'number'
+et_dict['column_name5'] = 'date'
 
-eu_dict = {}
-eu_dict['colunm_name1'] = 'int'
-eu_dict['colunm_name2'] = 'varchar'
-eu_dict['colunm_name3'] = 'boolean'
-eu_dict['colunm_name4'] = 'number'
-eu_dict['colunm_name5'] = 'date'
+eu_dict =OrderedDict()
+eu_dict['column_name1'] = 'pk'
+eu_dict['column_name2'] = 'varchar'
+eu_dict['column_name3'] = 'boolean'
+eu_dict['column_name4'] = 'number'
+eu_dict['column_name5'] = 'date'
 
-pm_dict = {}
-pm_dict['colunm_name1'] = 'int'
-pm_dict['colunm_name2'] = 'varchar'
-pm_dict['colunm_name3'] = 'boolean'
-pm_dict['colunm_name4'] = 'number'
-pm_dict['colunm_name5'] = 'date'
+pm_dict =OrderedDict()
+pm_dict['column_name1'] = 'pk'
+pm_dict['column_name2'] = 'varchar'
+pm_dict['column_name3'] = 'boolean'
+pm_dict['column_name4'] = 'number'
+pm_dict['column_name5'] = 'date'
 
-cfi_dict = {}
-cfi_dict['column_name1'] = 'int'
+cfi_dict =OrderedDict()
+cfi_dict['column_name1'] = 'pk'
 cfi_dict['column_name2'] = 'varchar'
 cfi_dict['column_name3'] = 'boolean'
 cfi_dict['column_name4'] = 'number'
 cfi_dict['column_name5'] = 'date'
 
-fl_dict = {}
-fl_dict['column_name1'] = 'int'
+fl_dict =OrderedDict()
+fl_dict['column_name1'] = 'pk'
 fl_dict['column_name2'] = 'varchar'
 fl_dict['column_name3'] = 'boolean'
 fl_dict['column_name4'] = 'number'
@@ -61,39 +63,62 @@ def get_int(size=3, chars=string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def get_varchar(size=6, chars=string.ascii_uppercase):
-    return ''.join(random.choice(chars) for _ in range(size))
+    return '"' + ''.join(random.choice(chars) for _ in range(size)) + '"'
 
 def get_number(size=3, chars=string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+    return '"' + ''.join(random.choice(chars) for _ in range(size)) + '"'
 
 def get_date():
-    return '01/01/1900'
+    now = datetime.datetime.now()
+    return '"' + now.strftime("%Y-%m-%d") + '"'
 
 def get_boolean():
     return random.choice([True, False])
 
 
 
-def generate_OP():
-    print 'generating OP'
+def generate_OP(f, count):
+    for i in range(int(count)):
+        f.write(generate_insert(op_dict, 'OP_TABLE_NAME'),)
+        f.write(generate_values(op_dict, i+1))
+        f.write('\n\n')
 
-def generate_FCT():
-    print 'generating FCT'
+def generate_FCT(f, count):
+    for i in range(int(count)):
+        f.write(generate_insert(fct_dict, 'FCT_TABLE_NAME'),)
+        f.write(generate_values(fct_dict, i+1))
+        f.write('\n\n')
 
-def generate_ET():
-    print 'generating ET'
+def generate_ET(f, count):
+    for i in range(int(count)):
+        f.write(generate_insert(et_dict, 'ET_TABLE_NAME'),)
+        f.write(generate_values(et_dict, i+1))
+        f.write('\n\n')
 
-def generate_EU():
-    print 'generating EU'
+def generate_EU(f, count):
+    for i in range(int(count)):
+        f.write(generate_insert(eu_dict, 'EU_TABLE_NAME'),)
+        f.write(generate_values(eu_dict, i+1))
+        f.write('\n\n')
 
-def generate_PM():
-    print 'generating PM'
+def generate_PM(f, count):
+    for i in range(int(count)):
+        f.write(generate_insert(pm_dict, 'PM_TABLE_NAME'),)
+        f.write(generate_values(pm_dict, i+1))
+        f.write('\n\n')
 
-def generate_CFI():
-    print 'generating CFI'
+def generate_CFI(f, count):
+    for i in range(int(count)):
+        f.write(generate_insert(cfi_dict, 'CFI_TABLE_NAME'),)
+        f.write(generate_values(cfi_dict, i+1))
+        f.write('\n\n')
 
-def generate_FL():
-    print 'generating FL'
+def generate_FL(f, count):
+    for i in range(int(count)):
+        f.write(generate_insert(fl_dict, 'FL_TABLE_NAME'),)
+        f.write(generate_values(fl_dict, i+1))
+        f.write('\n\n')
+
 
 # --------------------------------------------------------------------------------
 
@@ -103,7 +128,7 @@ def generate_insert(dict, table_name):
     sql += "("
     col_names = ""
     for k, v in dict.iteritems():
-        col_names += k
+        col_names += '"' + k + '"'
         col_names += ', '
 
     col_names = col_names[:-2]
@@ -129,12 +154,12 @@ def generate_insert(dict, table_name):
 #            sql += sql_template
 #    print sql
 
-def generate_values(dict):
+def generate_values(dict, pk):
     #print 'generating VALUES'
     col_values = ") values ("
     for k, v in dict.iteritems():
-        if v == 'int':
-            col_values += get_int()
+        if v == 'pk':
+            col_values += str(pk)
             col_values += ', '
         elif v == 'varchar':
             col_values += get_varchar()
@@ -161,5 +186,14 @@ def generate_values(dict):
 # --------------------------------------
 #   MAIN PROGRAM
 # --------------------------------------
-print generate_insert(op_dict, 'OP_TABLE_NAME'),
-print generate_values(op_dict)
+f= open("insert.sql","w+")
+generate_OP(f, 10)
+generate_FCT(f, 6)
+generate_ET(f, 6)
+generate_EU(f, 6)
+generate_PM(f, 6)
+generate_CFI(f, 6)
+generate_FL(f, 6)
+
+# Close file when done
+f.close()
